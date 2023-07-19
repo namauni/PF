4b46def177318a1693b902de07903c6b38596d37
EC2 Default User <ec2-user@ip-172-31-4-1.ap-northeast-1.compute.internal>
2023-07-19 18:35:56 +0000

    レイアウト更新

--- app/models/content_quesition_tag.rb
+++ app/models/content_quesition_tag.rb
@@ -2,4 +2,11 @@ class ContentQuesitionTag < ApplicationRecord
 belongs_to :content, optional: true
 belongs_to :question, optional: true
 belongs_to :tag
+
+  validates :content_or_question, presence: true
+
+  private
+    def content_or_question
+      content_id.presence or question_id.presence
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
+  
 devise_for :users
 root to: 'homes#top'
 get '/home/about' => 'homes#about',as: "about"
@@ -8,6 +7,6 @@ resources :contents, only: [:new, :create, :index, :show, :destroy, :edit, :upda
   resource :favorites, only: [:create, :destroy]
   resources :comments, only: [:create, :destroy]
 end
-resources :users, only: [:show, :edit]
+resources :users, only: [:show, :edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
