24c2bfca569b0383433fb5bf635ebf18ff3fec19
EC2 Default User <namauni0628@gmail.com>
2023-10-11 14:46:23 +0000

    タイムライン完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @followings = Content.where(user_id: [*current_user.following_ids])
   end
 
   def bookmarks_index
